0155e7d4a942aeba9ff85d598e6b35c7bc665ae8
Javier <Javier@Curro>
2017-06-12 10:09:49 +0200

    He cambiado los comentarios

--- src/paqueteED/elementosJava/Frutas.java
+++ src/paqueteED/elementosJava/Frutas.java
@@ -4,7 +4,7 @@ package paqueteED.elementosJava;
  * La clase Fruta es una clase patrón
  * no va a tener método main
  * tendrá 4 atributos o características
- * @author Pilar y Javier 
+ * @author Pilar y el resto
  * @version 1.0
  *
  */
