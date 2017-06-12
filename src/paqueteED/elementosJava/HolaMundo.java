dda1f2db2303c49e515a04feeb5f9f0dbd262bd7
Juan <Juan@DESKTOP-BQ0JA86>
2017-06-12 11:06:34 +0200

    modificado systemoutprintln

--- src/paqueteED/elementosJava/HolaMundo.java
+++ src/paqueteED/elementosJava/HolaMundo.java
@@ -3,7 +3,7 @@ package paqueteED.elementosJava;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo, Clase");
 
 	}
 
